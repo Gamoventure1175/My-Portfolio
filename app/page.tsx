40f496883011dc7ad6d641fe32380176b26cf736
Gaurav Abhiman Mahajan <gauravmahajan1175@gmail.com>
2026-07-15 20:02:22 +0530

    Starting from the beginning

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-import Hero from "@/components/sections/Hero";
 import NamePanel from "@/components/ui/NamePanel";
 import NavBar from "@/components/ui/NavBar";
-import ExperienceSection from "@/components/sections/ExperienceSection";
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
       <NamePanel />
-      <Hero />
-      <ExperienceSection />
     </main>
 
   );
